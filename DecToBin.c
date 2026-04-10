33a14caf08a231f3cbbb2222065c67258181c23b
Alexander <pchp250g8@tutamail.com>
2026-04-10 14:40:59 +0300

    Fixed Source Code

--- DecToBin.c
+++ DecToBin.c
@@ -8,10 +8,18 @@
 
 int _tmain(int argc, _TCHAR* argv[]) 
 {
-    uint32_t uDecNumber, uTempVal,nBinaryDigits = 0;
+    long uDecNumber, uTempVal;
+    unsigned int nBinaryDigits = 0;
     char* szBinaryNum;
     printf("Input an unsigned integer number\r\n");
-    fscanf(stdin, "%u", &uDecNumber);
+    scanf("%lu", &uDecNumber);
+    if (uDecNumber < 0 || uDecNumber > UINT32_MAX)
+    {
+        printf("Incorrect number format\r\n");
+        getchar();
+        getc(stdin);
+        return 0;
+    }
     uTempVal = uDecNumber;
     while (uTempVal > 0)
     {
@@ -24,7 +32,7 @@ int _tmain(int argc, _TCHAR* argv[])
         szBinaryNum = (char*)calloc(nBinaryDigits + 1, sizeof(char));
         if (szBinaryNum == NULL) return -1;
         _strset(szBinaryNum, 0);
-        for (uint32_t i = 0; i < nBinaryDigits; i++)
+        for (unsigned int i = 0; i < nBinaryDigits; i++)
         {
             char chBinDighit = '0' + (uTempVal % 2);
             szBinaryNum[nBinaryDigits - 1 - i] = chBinDighit;
@@ -32,14 +40,18 @@ int _tmain(int argc, _TCHAR* argv[])
         }
         printf
         (
-        	"The binary equivalent of the numner %u is %s\r\n",
+        	"The binary equivalent of the decimal numner %lu is %s\r\n",
             uDecNumber,szBinaryNum
         );
         free(szBinaryNum);
     }
     else
     {
-        printf("The binary equivalent of the number %u is 0\r\n", uDecNumber);
+        printf
+        (
+        	"The binary equivalent of the decimal number %lu is 0\r\n",
+            uDecNumber
+        );
     }
     getchar();
     getc(stdin);
